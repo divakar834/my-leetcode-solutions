c0fc3fad7ca060bed3ff2d710bf32dbaafc6bb2f
Divakaran P <divakarpalani73@gmail.com>
2026-08-31 10:14:43 +0530

    Time: 0 ms (100%), Space: 19.3 MB (37.67%) - LeetHub

---
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def increasingBST(self, root):
+        def sortBST(node):
+            if not node:    return []
+            return sortBST(node.left) + [node.val] + sortBST(node.right)
+        sorted_list = sortBST(root)
+        ans = temp = TreeNode(sorted_list[0])
+        for i in range(1, len(sorted_list)):
+            temp.right = TreeNode(sorted_list[i])
+            temp = temp.right
+        return ans
