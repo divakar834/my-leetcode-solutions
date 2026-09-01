d91509e841178195d261abc847306c22174cb021
Divakaran P <divakarpalani73@gmail.com>
2026-09-01 14:20:59 +0530

    Time: 3 ms (49.85%), Space: 27.8 MB (46.37%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,41 @@
+class Solution:
+    def reorderList(self, head):
+
+        if not head or not head.next:
+            return
+
+        # Step 1: Find Middle
+        slow = head
+        fast = head
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: Split List
+        second = slow.next
+        slow.next = None
+
+        # Step 3: Reverse Second Half
+        prev = None
+
+        while second:
+            nxt = second.next
+            second.next = prev
+            prev = second
+            second = nxt
+
+        second = prev
+
+        # Step 4: Merge
+        first = head
+
+        while second:
+            temp1 = first.next
+            temp2 = second.next
+
+            first.next = second
+            second.next = temp1
+
+            first = temp1
+            second = temp2
